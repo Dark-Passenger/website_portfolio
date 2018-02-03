93c32ca2c584baa6e6cb12feefd504120d7dc762
Dhruv Paranjape <lord.dhruv@gmail.com>
2018-02-03 13:43:44 +0530

    change contact page to display a success message on completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -22,7 +23,9 @@ import { SmoothScrollDirective } from './smooth-scroll.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormArray } from '@angular/forms/src/model';
 
 @Component({
   selector: 'app-contact',
@@ -8,8 +10,44 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() { }
+  
+  not_submitted: boolean;
+  submitted: boolean;
+  myform: FormGroup;
+  name: FormControl;
+  email: FormControl;
+  phone: FormControl;
+  message: FormControl;
 
   ngOnInit() {
+    this.submitted = false;
+    this.createFormControls();
+    this.message = new FormControl();
+    this.createForm();
+  }
+  
+  onSubmit() {
+    if (this.myform.valid) {
+      console.log("Form Submitted!");
+      console.log(this.myform.value);
+      this.myform.reset();
+      this.submitted = true;
+    }
   }
 
+  createFormControls() {
+    this.name = new FormControl('', Validators.required);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.message = new FormControl('', Validators.required);
+    this.phone = new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(14), Validators.pattern('^[0-9]*$')]);
+  }
+
+  createForm() {
+    this.myform = new FormGroup({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+      phone: this.phone
+    });
+  }
 }
